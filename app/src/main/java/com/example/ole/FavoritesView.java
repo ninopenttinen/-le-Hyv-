5a432172d3cca62b0ad7212fd255e95280bb1858
jooPen <joo.pennanen@gmail.com>
2021-11-12 13:31:04 +0200

    ImageOnClick & Remove dialog

--- app/src/main/java/com/example/ole/FavoritesView.java
+++ app/src/main/java/com/example/ole/FavoritesView.java
@@ -1,14 +1,18 @@
 package com.example.ole;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Layer;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 
 import com.example.ole.components.CategoryItem;
@@ -18,6 +22,7 @@ import com.example.ole.database.AppDatabase;
 import com.example.ole.model.Recipe;
 import com.example.ole.roomsitems.RoomRecipe;
 import com.example.ole.roomsitems.RoomRecipeWithIngredients;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,6 +48,15 @@ public class FavoritesView extends AppCompatActivity {
 
         updateFavorites(recipeWithIngredients);
 
+
+        ImageButton imageButton = findViewById(R.id.fav_actions_button);
+         /*  imageButton.setOnClickListener(v -> {
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(FavoritesView.this, R.style.BottomSheetDialogTheme);
+            View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.bottom_popup_layout,
+                    (LinearLayout)findViewById(R.id.bottomSheetContainer));
+            *//*bottomSheetView.findViewById(R.id.)*//*
+        });
+*/
     }
 
     private void updateFavorites(List<RoomRecipeWithIngredients> favRecipe){
@@ -68,10 +82,24 @@ public class FavoritesView extends AppCompatActivity {
         favListView.setAdapter(simpleAdapter);
     }
 
-  
-  
-  
-                     
-  
+    public void favClick(View v){
 
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(FavoritesView.this, R.style.BottomSheetDialogTheme);
+        View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.bottom_popup_layout,
+                (LinearLayout)findViewById(R.id.bottomSheetContainer));
+
+        bottomSheetView.findViewById(R.id.cancel_button).setOnClickListener(v1 -> {
+            Toast.makeText(FavoritesView.this, "Canceled", Toast.LENGTH_LONG).show();
+            bottomSheetDialog.dismiss();
+        });
+
+        bottomSheetView.findViewById(R.id.confirm_button).setOnClickListener(v1 -> {
+            Toast.makeText(FavoritesView.this, "Recipe removed", Toast.LENGTH_LONG).show();
+
+            bottomSheetDialog.dismiss();
+        });
+
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+    }
 }
