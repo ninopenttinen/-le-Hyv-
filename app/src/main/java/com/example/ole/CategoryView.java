f04d7056ec11c1f7414ab5c103ea47c7fd2aeef1
Jantero93 <janzku1993@hotmail.com>
2021-11-16 15:16:10 +0200

    MOB-13 pass category via intent categoryview -> suggestionview

--- app/src/main/java/com/example/ole/CategoryView.java
+++ app/src/main/java/com/example/ole/CategoryView.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -29,20 +30,15 @@ public class CategoryView extends AppCompatActivity {
   }
 
   private void setCategoryView() {
-    int categoryCount = getResources()
-        .obtainTypedArray(R.array.cuisine_types_string)
-        .length();
+    int categorySize = getResources().getStringArray(R.array.cuisine_types_string).length;
 
-    for (int i = 0; i < categoryCount; i++) {
+    for (int i = 0; i < categorySize; i++) {
       String categoryImage = Integer.toString(
           getResources()
               .obtainTypedArray(R.array.cuisine_images_ref)
               .getResourceId(i, 0)
       );
-
-      String categoryName = getResources()
-          .obtainTypedArray(R.array.cuisine_types_string)
-          .getString(i);
+      String categoryName = getResources().getStringArray(R.array.cuisine_types_string)[i];
 
       categoryItemArrayList.add(new CategoryItem(categoryName, categoryImage));
     }
@@ -59,15 +55,16 @@ public class CategoryView extends AppCompatActivity {
         new int[]{R.id.categoryImageView, R.id.categoryTextView}
     );
 
-    // Add adapter to gridView
     GridView categoryGridView = (GridView) findViewById(R.id.gridView);
     categoryGridView.setAdapter(simpleAdapter);
-  }
 
-  public void categoryOnClick(View view) {
-    Intent intent = new Intent(this, SuggestionsView.class);
-    intent.putExtra("category", "chinese"); // TODO: add value from parameters
-    startActivity(intent);
+    categoryGridView.setOnItemClickListener((parent, view, position, id) -> {
+      String category = getResources().getStringArray(R.array.cuisine_types_string)[position];
+
+      Intent intent = new Intent(this, SuggestionsView.class);
+      intent.putExtra("category", category);
+      startActivity(intent);
+    });
   }
 
   public void settingsOnClick(View view) {
